c1b9145f6f03981cd28648c1a7fdf34f14fb24bc
Michael W. Powell <mwpowelllde@gmail.com>
2021-02-05 16:36:10 -0500

    Added a LINQ style select function.

--- Missionframework/modules/0015_linq/functions.hpp
+++ Missionframework/modules/0015_linq/functions.hpp
@@ -4,7 +4,7 @@
     File: functions.hpp
     Author: Michael W. Powell [22nd MEU SOC]
     Created: 2021-01-28 02:37:45
-    Last Update: 2021-01-28 02:37:48
+    Last Update: 2021-02-05 16:30:22
     License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
 
     Description:
@@ -22,6 +22,9 @@ class linq {
 
     // Provides a LINQ style zip function
     class linq_last {};
+
+    // Provides a LINQ style select function
+    class linq_select {};
 
     // Provides a LINQ style zip function
     class linq_zip {};
