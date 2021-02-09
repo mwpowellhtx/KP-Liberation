c4177f5a79d92f9ccbc18cd4c205451a8bfd00c7
Michael W. Powell <mwpowelllde@gmail.com>
2021-02-09 18:17:23 -0500

    Stubbed in the production manager queue action buttons. Also anticipating real estate for the time remaining bits.

--- Missionframework/modules/0415_productionmgr/ui/KPLIB_productionMgr.hpp
+++ Missionframework/modules/0415_productionmgr/ui/KPLIB_productionMgr.hpp
@@ -19,10 +19,12 @@
 #define KPLIB_PRODUCTIONMGR_TITLE_XC            KPLIB_PRODUCTIONMGR_DIALOG_XC
 #define KPLIB_PRODUCTIONMGR_CROSS_XC            (KPLIB_PRODUCTIONMGR_TITLE_XC + KPLIB_PRODUCTIONMGR_TITLE_WC - GUI_GRID_W)
 
+#define KPLIB_PRODUCTIONMGR_BTN_H               KPX_BUTTON_M_H
+
 #define KPLIB_PRODUCTIONMGR_CTRLAREA_XC         KPX_GETXL_VXW(KPLIB_PRODUCTIONMGR_DIALOG_XC,KPX_SPACING_W)
 #define KPLIB_PRODUCTIONMGR_CTRLAREA_YC         KPX_DEFAULT_CTRLAREA_YC
 
-#define KPLIB_PRODUCTIONMGR_LNBSECTORS_H        (KPX_DEFAULT_DIALOG_HC - (3 * (KPX_BUTTON_M_H + KPX_SPACING_H)))
+#define KPLIB_PRODUCTIONMGR_LNBSECTORS_H        (KPLIB_PRODUCTIONMGR_DIALOG_HC - (3 * (KPLIB_PRODUCTIONMGR_BTN_H + KPX_SPACING_H)))
 #define KPLIB_PRODUCTIONMGR_LNBSECTORS_W        KPX_GETW_VWGS(KPLIB_PRODUCTIONMGR_DIALOG_WC,8,25,KPX_SPACING_W)
 
 // Four rows, three status rows plus one 'header' row
@@ -38,8 +40,27 @@
 #define KPLIB_PRODUCTIONMGR_LNBQUEUE_XC         KPLIB_PRODUCTIONMGR_LNBSTATUS_XC
 #define KPLIB_PRODUCTIONMGR_LNBQUEUE_YC         KPX_GETYT_VYH(KPLIB_PRODUCTIONMGR_LNBSTATUS_YC,(KPX_SPACING_H + KPLIB_PRODUCTIONMGR_LNBSTATUS_H))
 
+#define KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_W      KPX_GETW_VWGS(KPLIB_PRODUCTIONMGR_DIALOG_WC,2,25,KPX_SPACING_W)
+
+/*
+    Description:
+        The queue list box will have several buttons accompanying it for purposes of
+        arranging the queue and interacting with it. The buttons will be arranged in
+        a grid like manner.
+
+        11111 22222 33333
+        44444 55555 66666
+ */
+
+// Align the queue buttons with respect to the queue list box
+#define KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_XL     KPX_GETXL_VXW(KPLIB_PRODUCTIONMGR_LNBQUEUE_XC,(KPLIB_PRODUCTIONMGR_LNBQUEUE_W + KPX_SPACING_W))
+#define KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_XM     KPX_GETXL_VXW(KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_XL,(KPX_SPACING_W + KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_W))
+#define KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_XR     KPX_GETXL_VXW(KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_XM,(KPX_SPACING_W + KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_W))
+
+#define KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_GETDELTAH(BY)  (KPLIB_PRODUCTIONMGR_LNBQUEUE_H - ((BY + 1) * KPLIB_PRODUCTIONMGR_BTN_H) - (BY * KPX_SPACING_H))
+
 // Math from the bottom of the dialog up, which simplifies the substitution throughout as well.
-#define KPLIB_PRODUCTIONMGR_BTN_GETDELTAH(BY)   (KPX_DEFAULT_DIALOG_HC - ((BY + 1) * KPX_BUTTON_M_H) - (BY * KPX_SPACING_H))
+#define KPLIB_PRODUCTIONMGR_BTN_GETDELTAH(BY)   (KPX_DEFAULT_DIALOG_HC - ((BY + 1) * KPLIB_PRODUCTIONMGR_BTN_H) - (BY * KPX_SPACING_H))
 
 class KPLIB_productionMgr {
     idd = KPLIB_IDD_PRODUCTIONMGR;
@@ -63,70 +84,6 @@ class KPLIB_productionMgr {
             x = KPLIB_PRODUCTIONMGR_DIALOG_XC;
             w = KPLIB_PRODUCTIONMGR_DIALOG_WC;
         };
-
-        // Tools controlsGroup
-
-        // class KPLIB_GroupTools : KPGUI_PRE_ControlsGroupNoScrollbars {
-        //     idc = KPLIB_IDC_CRATEFILLER_GROUPOVERVIEW;
-        //     x = safezoneX;
-        //     y = safezoneY;
-        //     w = safezoneW;
-        //     h = safezoneH;
-
-        //     class controls {
-
-        //         class KPLIB_DialogTitleTools: KPGUI_PRE_DialogTitleSR {
-        //             text = "$STR_KPLIB_DIALOG_CRATEFILLER_TITLEOVERVIEW";
-        //             x = KP_GETX(KP_X_VAL_SR,KP_WIDTH_VAL_SR,0,1) - safezoneX;
-        //             y = safeZoneY + safeZoneH * KP_Y_VAL_SR - safezoneY;
-        //         };
-
-        //         class KPLIB_DialogAreaTools: KPGUI_PRE_DialogBackgroundSR {
-        //             x = KP_GETX(KP_X_VAL_SR,KP_WIDTH_VAL_SR,0,1) - safezoneX;
-        //             y = KP_GETY_AREA(KP_Y_VAL_SR) - safezoneY;
-        //         };
-
-        //         class KPLIB_ComboGroups: KPGUI_PRE_Combo {
-        //             idc = KPLIB_IDC_CRATEFILLER_COMBOGROUPS;
-        //             x = KP_GETCX(KP_X_VAL_SR,KP_WIDTH_VAL_SR,0,1) - safezoneX;
-        //             y = KP_GETCY(KP_Y_VAL_SR,KP_HEIGHT_VAL_SR,0,48) - safezoneY;
-        //             w = KP_GETW(KP_WIDTH_VAL_SR,1);
-        //             h = KP_GETH(KP_HEIGHT_VAL_SR,24);
-        //             tooltip = "$STR_KPLIB_DIALOG_CRATEFILLER_GROUPS_TT";
-        //             onLBSelChanged = "[] call KPLIB_fnc_cratefiller_getPlayers";
-        //         };
-
-        //         class KPLIB_ComboPlayers: KPLIB_ComboGroups {
-        //             idc = KPLIB_IDC_CRATEFILLER_COMBOPLAYERS;
-        //             y = KP_GETCY(KP_Y_VAL_SR,KP_HEIGHT_VAL_SR,2,48) - safezoneY;
-        //             tooltip = "$STR_KPLIB_DIALOG_CRATEFILLER_PLAYERS_TT";
-        //             onLBSelChanged = "[] call KPLIB_fnc_cratefiller_getPlayerInventory";
-        //         };
-
-        //         class KPLIB_MainWeapon: KPGUI_PRE_PictureRatio {
-        //             idc = KPLIB_IDC_CRATEFILLER_MAINWEAPON;
-        //             text = "\A3\Ui_f\data\GUI\Rsc\RscDisplayArsenal\PrimaryWeapon_ca.paa";
-        //             x = KP_GETCX(KP_X_VAL_SR,KP_WIDTH_VAL_SR,0,1) - safezoneX;
-        //             y = KP_GETCY(KP_Y_VAL_SR,KP_HEIGHT_VAL_SR,6,48) - safezoneY;
-        //             w = KP_GETW(KP_WIDTH_VAL_SR,1);
-        //             h = KP_GETH(KP_HEIGHT_VAL_SR,4);
-        //         };
-
-        //         class KPLIB_Handgun: KPLIB_MainWeapon {
-        //             idc = KPLIB_IDC_CRATEFILLER_HANDGUN;
-        //             text = "\A3\Ui_f\data\GUI\Rsc\RscDisplayArsenal\Handgun_ca.paa";
-        //             y = KP_GETCY(KP_Y_VAL_SR,KP_HEIGHT_VAL_SR,21,48) - safezoneY;
-        //         };
-
-        //         class KPLIB_SecondaryWeapon: KPLIB_MainWeapon {
-        //             idc = KPLIB_IDC_CRATEFILLER_SECONDARYWEAPONS;
-        //             text = "\A3\Ui_f\data\GUI\Rsc\RscDisplayArsenal\SecondaryWeapon_ca.paa";
-        //             y = KP_GETCY(KP_Y_VAL_SR,KP_HEIGHT_VAL_SR,36,48) - safezoneY;
-        //         };
-        //     };
-        // };
-
-        // Tools controlsGroup end
     };
 
     class controls {
@@ -188,191 +145,77 @@ class KPLIB_productionMgr {
             onLBDblClick = "_this call KPLIB_fnc_productionMgr_lnbQueue_onLBDblClick";
         };
 
-        // class KPLIB_ButtonTools: KPGUI_PRE_DialogCrossS {
-        //     idc = KPLIB_IDC_CRATEFILLER_BUTTONOVERVIEW;
-        //     text = "KPGUI\res\icon_tools.paa";
-        //     x = safeZoneX + safeZoneW * (KP_X_VAL_S + KP_WIDTH_VAL_S - 0.04);
-        //     tooltip = "$STR_KPLIB_DIALOG_CRATEFILLER_OVERVIEW_TT";
-        //     action = "[] call KPLIB_fnc_cratefiller_showOverview";
-        // };
+        class KPLIB_ctrl_btnRemove : XGUI_PRE_Button {
+            x = KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_XL;
+            y = KPX_GETYT_VYH(KPLIB_PRODUCTIONMGR_LNBQUEUE_YC,KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_GETDELTAH(1));
+            w = KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_W;
 
-        // // Equipment
+            // TODO: TBD: refactor to string table... can unicode be supported (?)
+            text = ">";
+            sizeEx = KPX_TEXT_S;
 
-        // class KPLIB_EquipmentTitle: KPGUI_PRE_InlineTitle {
-        //     text = "$STR_KPLIB_DIALOG_CRATEFILLER_TITLEEQUIPMENT";
-        //     x = KP_GETCX(KP_X_VAL_S,KP_WIDTH_VAL_S,0,2);
-        //     y = KP_GETCY(KP_Y_VAL_S,KP_HEIGHT_VAL_S,0,48);
-        //     w = KP_GETW(KP_WIDTH_VAL_S,2);
-        //     h = KP_GETH(KP_HEIGHT_VAL_S,16);
-        // };
+            onLoad = "_this call KPLIB_fnc_productionMgr_onLoad_debug";
+        };
 
-        // class KPLIB_ComboEquipment: KPGUI_PRE_Combo {
-        //     idc = KPLIB_IDC_CRATEFILLER_COMBOEQUIPMENT;
-        //     x = KP_GETCX(KP_X_VAL_S,KP_WIDTH_VAL_S,0,1);
-        //     y = KP_GETCY(KP_Y_VAL_S,KP_HEIGHT_VAL_S,3,48);
-        //     w = KP_GETW(KP_WIDTH_VAL_S,2);
-        //     h = KP_GETH(KP_HEIGHT_VAL_S,24);
-        //     tooltip = "$STR_KPLIB_DIALOG_CRATEFILLER_CATEGORY_TT";
-        //     onLBSelChanged = "[] call KPLIB_fnc_cratefiller_createEquipmentList";
-        // };
+        class KPLIB_ctrl_btnIncreasePriority : XGUI_PRE_Button {
+            x = KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_XM;
+            y = KPX_GETYT_VYH(KPLIB_PRODUCTIONMGR_LNBQUEUE_YC,KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_GETDELTAH(1));
+            w = KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_W;
 
-        // class KPLIB_ComboWeapons: KPLIB_ComboEquipment {
-        //     idc = KPLIB_IDC_CRATEFILLER_COMBOWEAPONS;
-        //     y = KP_GETCY(KP_Y_VAL_S,KP_HEIGHT_VAL_S,5,48);
-        //     w = KP_GETW(KP_WIDTH_VAL_S,3);
-        //     tooltip = "$STR_KPLIB_DIALOG_CRATEFILLER_WEAPONSELECTION_TT";
-        //     onLBSelChanged = "[] call KPLIB_fnc_cratefiller_createSubList";
-        // };
+            // TODO: TBD: refactor to string table... can unicode be supported (?)
+            text = "+";
+            sizeEx = KPX_TEXT_S;
 
-        // class KPLIB_SearchBar: KPGUI_PRE_EditBox {
-        //     idc = KPLIB_IDC_CRATEFILLER_SEARCHBAR;
-        //     x = KP_GETCX(KP_X_VAL_S,KP_WIDTH_VAL_S,1,3);
-        //     y = KP_GETCY(KP_Y_VAL_S,KP_HEIGHT_VAL_S,5,48);
-        //     w = KP_GETW(KP_WIDTH_VAL_S,6);
-        //     h = KP_GETH(KP_HEIGHT_VAL_S,24);
-        //     tooltip = "$STR_KPLIB_DIALOG_CRATEFILLER_SEARCH_TT";
-        //     onKeyUp = "[] call KPLIB_fnc_cratefiller_search";
-        // };
+            onLoad = "(_this + [[1]]) call KPLIB_fnc_productionMgr_onLoad_debug";
+        };
 
-        // class KPLIB_LeftEquipmentListButton: KPGUI_PRE_BUTTON {
-        //     idc = KPLIB_IDC_CRATEFILLER_LEFTEQUIPMENTBUTTON;
-        //     text = "-";
-        //     onButtonClick = "[687413] call KPLIB_fnc_cratefiller_removeEquipment";
-        // };
+        class KPLIB_ctrl_btnDecreasePriority : XGUI_PRE_Button {
+            x = KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_XR;
+            y = KPX_GETYT_VYH(KPLIB_PRODUCTIONMGR_LNBQUEUE_YC,KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_GETDELTAH(1));
+            w = KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_W;
 
-        // class KPLIB_RightEquipmentListButton: KPGUI_PRE_BUTTON {
-        //     idc = KPLIB_IDC_CRATEFILLER_RIGHTEQUIPMENTBUTTON;
-        //     text = "+";
-        //     onButtonClick = "[687413] call KPLIB_fnc_cratefiller_addEquipment";
-        // };
+            // TODO: TBD: refactor to string table... can unicode be supported (?)
+            text = "-";
+            sizeEx = KPX_TEXT_S;
 
-        // class KPLIB_EquipmentList: KPGUI_PRE_ListNBox {
-        //     idc = KPLIB_IDC_CRATEFILLER_EQUIPMENTLIST;
-        //     x = KP_GETCX(KP_X_VAL_S,KP_WIDTH_VAL_S,0,1);
-        //     y = KP_GETCY(KP_Y_VAL_S,KP_HEIGHT_VAL_S,8,48);
-        //     w = KP_GETW(KP_WIDTH_VAL_S,2);
-        //     h = KP_GETH(KP_HEIGHT_VAL_S,(48/38));
+            onLoad = "(_this + [[-1]]) call KPLIB_fnc_productionMgr_onLoad_debug";
+        };
 
-        //     columns[] = {0.05, 0.2};
+        class KPLIB_ctrl_btnEnqueueSupply : XGUI_PRE_Button {
+            x = KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_XL;
+            y = KPX_GETYT_VYH(KPLIB_PRODUCTIONMGR_LNBQUEUE_YC,KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_GETDELTAH(0));
+            w = KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_W;
 
-        //     idcLeft = KPLIB_IDC_CRATEFILLER_LEFTEQUIPMENTBUTTON;
-        //     idcRight = KPLIB_IDC_CRATEFILLER_RIGHTEQUIPMENTBUTTON;
-        // };
+            // TODO: TBD: refactor to string table... can unicode be supported (?)
+            text = "< SUPPLY";
+            sizeEx = KPX_TEXT_S;
 
-        // // Crates
+            onLoad = "_this call KPLIB_fnc_productionMgr_onLoad_debug";
+        };
 
-        // class KPLIB_TransportTitle: KPLIB_EquipmentTitle {
-        //     text = "$STR_KPLIB_DIALOG_CRATEFILLER_TITLETRANSPORT";
-        //     x = KP_GETCX(KP_X_VAL_S,KP_WIDTH_VAL_S,1,2);
-        //     y = KP_GETCY(KP_Y_VAL_S,KP_HEIGHT_VAL_S,0,48);
-        // };
+        class KPLIB_ctrl_btnEnqueueAmmo : XGUI_PRE_Button {
+            x = KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_XM;
+            y = KPX_GETYT_VYH(KPLIB_PRODUCTIONMGR_LNBQUEUE_YC,KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_GETDELTAH(0));
+            w = KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_W;
 
-        // class KPLIB_ComboCargo: KPGUI_PRE_Combo {
-        //     idc = KPLIB_IDC_CRATEFILLER_COMBOCARGO;
-        //     x = KP_GETCX(KP_X_VAL_S,KP_WIDTH_VAL_S,1,2);
-        //     y = KP_GETCY(KP_Y_VAL_S,KP_HEIGHT_VAL_S,3,48);
-        //     w = KP_GETW(KP_WIDTH_VAL_S,(24/11));
-        //     h = KP_GETH(KP_HEIGHT_VAL_S,24);
-        //     tooltip = "$STR_KPLIB_DIALOG_CRATEFILLER_INVENTORY_TT";
-        //     onLBSelChanged = "[] call KPLIB_fnc_cratefiller_showInventory";
-        // };
+            // TODO: TBD: refactor to string table... can unicode be supported (?)
+            text = "< AMMO";
+            sizeEx = KPX_TEXT_S;
 
-        // class KPLIB_RefreshCargo: KPGUI_PRE_CloseCross {
-        //     text = "KPGUI\res\icon_refresh.paa";
-        //     x = KP_GETCX(KP_X_VAL_S,KP_WIDTH_VAL_S,23,24);
-        //     y = KP_GETCY(KP_Y_VAL_S,KP_HEIGHT_VAL_S,3,48);
-        //     w = KP_GETW(KP_WIDTH_VAL_S,24);
-        //     h = KP_GETH(KP_HEIGHT_VAL_S,24);
-        //     tooltip = "$STR_KPLIB_DIALOG_CRATEFILLER_REFRESH_TT";
-        //     action = "[] call KPLIB_fnc_cratefiller_getNearStorages";
-        // };
+            onLoad = "_this call KPLIB_fnc_productionMgr_onLoad_debug";
+        };
 
-        // // Inventory
+        class KPLIB_ctrl_btnEnqueueFuel : XGUI_PRE_Button {
+            x = KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_XR;
+            y = KPX_GETYT_VYH(KPLIB_PRODUCTIONMGR_LNBQUEUE_YC,KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_GETDELTAH(0));
+            w = KPLIB_PRODUCTIONMGR_LNBQUEUE_BTN_W;
 
-        // class KPLIB_InventoryTitle: KPLIB_TransportTitle {
-        //     text = "$STR_KPLIB_DIALOG_CRATEFILLER_TITLEINVENTORY";
-        //     y = KP_GETCY(KP_Y_VAL_S,KP_HEIGHT_VAL_S,5,48);
-        // };
+            // TODO: TBD: refactor to string table... can unicode be supported (?)
+            text = "< FUEL";
+            sizeEx = KPX_TEXT_S;
 
-        // class KPLIB_ExportName: KPLIB_SearchBar {
-        //     idc = KPLIB_IDC_CRATEFILLER_EXPORTNAME;
-        //     x = KP_GETCX(KP_X_VAL_S,KP_WIDTH_VAL_S,2,4);
-        //     y = KP_GETCY(KP_Y_VAL_S,KP_HEIGHT_VAL_S,8,48);
-        //     w = KP_GETW(KP_WIDTH_VAL_S,4);
-        //     tooltip = "$STR_KPLIB_DIALOG_CRATEFILLER_EXPORT_TT";
-        // };
-
-        // class KPLIB_ImportName: KPGUI_PRE_Combo {
-        //     idc = KPLIB_IDC_CRATEFILLER_IMPORTNAME;
-        //     x = KP_GETCX(KP_X_VAL_S,KP_WIDTH_VAL_S,3,4);
-        //     y = KP_GETCY(KP_Y_VAL_S,KP_HEIGHT_VAL_S,8,48);
-        //     w = KP_GETW(KP_WIDTH_VAL_S,4);
-        //     h = KP_GETH(KP_HEIGHT_VAL_S,24);
-        //     tooltip = "$STR_KPLIB_DIALOG_CRATEFILLER_IMPORT_TT";
-        // };
-
-        // class KPLIB_ButtonExport: KPGUI_PRE_InlineButton {
-        //     text = "Export";
-        //     x = KP_GETCX(KP_X_VAL_S,KP_WIDTH_VAL_S,2,4);
-        //     y = KP_GETCY(KP_Y_VAL_S,KP_HEIGHT_VAL_S,10,48);
-        //     w = KP_GETW(KP_WIDTH_VAL_S,4);
-        //     h = KP_GETH(KP_HEIGHT_VAL_S,24);
-        //     onButtonClick = "[] call KPLIB_fnc_cratefiller_export";
-        // };
-
-        // class KPLIB_ButtonImport: KPLIB_ButtonExport {
-        //     text = "Import";
-        //     x = KP_GETCX(KP_X_VAL_S,KP_WIDTH_VAL_S,3,4);
-        //     w = KP_GETW(KP_WIDTH_VAL_S,(24/5));
-        //     onButtonClick = "[] call KPLIB_fnc_cratefiller_import";
-        // };
-
-        // class KPLIB_DeletePreset: KPGUI_PRE_CloseCross {
-        //     text = "KPGUI\res\icon_recyclebin.paa";
-        //     x = KP_GETCX(KP_X_VAL_S,KP_WIDTH_VAL_S,23,24);
-        //     y = KP_GETCY(KP_Y_VAL_S,KP_HEIGHT_VAL_S,10,48);
-        //     w = KP_GETW(KP_WIDTH_VAL_S,24);
-        //     h = KP_GETH(KP_HEIGHT_VAL_S,24);
-        //     tooltip = "$STR_KPLIB_DIALOG_CRATEFILLER_DELETE_TT";
-        //     action = "[] call KPLIB_fnc_cratefiller_deletePreset";
-        // };
-
-        // class KPLIB_LeftInventoryListButton: KPGUI_PRE_BUTTON {
-        //     idc = KPLIB_IDC_CRATEFILLER_LEFTINVENTORYBUTTON;
-        //     text = "-";
-        //     onButtonClick = "[687418] call KPLIB_fnc_cratefiller_removeEquipment";
-        // };
-
-        // class KPLIB_RightInventoryListButton: KPGUI_PRE_BUTTON {
-        //     idc = KPLIB_IDC_CRATEFILLER_RIGHTINVENTORYBUTTON;
-        //     text = "+";
-        //     onButtonClick = "[687418] call KPLIB_fnc_cratefiller_addEquipment";
-        // };
-
-        // class KPLIB_InventoryList: KPGUI_PRE_ListNBox {
-        //     idc = KPLIB_IDC_CRATEFILLER_INVENTORYLIST;
-        //     x = KP_GETCX(KP_X_VAL_S,KP_WIDTH_VAL_S,1,2);
-        //     y = KP_GETCY(KP_Y_VAL_S,KP_HEIGHT_VAL_S,12,48);
-        //     w = KP_GETW(KP_WIDTH_VAL_S,2);
-        //     h = KP_GETH(KP_HEIGHT_VAL_S,(48/34));
-
-        //     columns[] = {0.05, 0.2, 0.3};
-
-        //     idcLeft = KPLIB_IDC_CRATEFILLER_LEFTINVENTORYBUTTON;
-        //     idcRight = KPLIB_IDC_CRATEFILLER_RIGHTINVENTORYBUTTON;
-        // };
-
-        // class KPLIB_ProgressBar : KPGUI_PRE_ProgressBar {
-        //     idc = KPLIB_IDC_CRATEFILLER_PROGRESSBAR;
-        //     x = KP_GETCX(KP_X_VAL_S,KP_WIDTH_VAL_S,0,1);
-        //     y = KP_GETCY(KP_Y_VAL_S,KP_HEIGHT_VAL_S,46,48);
-        //     w = KP_GETW(KP_WIDTH_VAL_S,1);
-        //     h = KP_GETH(KP_HEIGHT_VAL_S,24);
-        //     tooltip = "$STR_KPLIB_DIALOG_CRATEFILLER_FILLLEVEL_TT";
-        // };
-
-        //class KPLIB_DialogCross : KPGUI_PRE_DialogCrossS {
-        //};
+            onLoad = "_this call KPLIB_fnc_productionMgr_onLoad_debug";
+        };
 
         class KPLIB_ctrl_btnRefresh : XGUI_PRE_Button {
             x = KPLIB_PRODUCTIONMGR_CTRLAREA_XC;
