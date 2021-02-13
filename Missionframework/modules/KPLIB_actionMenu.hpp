792184042f7e29bb76ae8fe2960428b132a22a83
Michael W. Powell <mwpowelllde@gmail.com>
2021-02-12 19:00:43 -0500

    Added additional action menu identifiers.

--- Missionframework/modules/KPLIB_actionMenu.hpp
+++ Missionframework/modules/KPLIB_actionMenu.hpp
@@ -2,7 +2,7 @@
     File: KPLIB_actionMenu.hpp
     Author: Michael W. Powell [22nd MEU SOC]
     Created: 2021-02-02 22:46:15
-    Last Update: 2021-02-02 22:46:18
+    Last Update: 2021-02-12 08:23:32
     License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
     Public: No
 
@@ -15,6 +15,10 @@
 
 /* Treating '.hpp' files as first class citizens in the code base...
  * See: https://flaviocopes.com/vscode-language-specific-settings */
+
+#define KPLIB_ACTION_PRIORITY_ASSETMOVE             (-700)
+
+#define KPLIB_ACTION_PRIORITY_DEPLOYFOB             (-800)
 
 #define KPLIB_ACTION_PRIORITY_REDEPLOY              (-801)
 #define KPLIB_ACTION_PRIORITY_ARSENAL               (-801)
@@ -32,6 +36,8 @@
 #define KPLIB_ACTION_PRIORITY_GARRISON_MANAGEMENT   (-824)
 
 #define KPLIB_ACTION_PRIORITY_ADMIN                 (-831)
+#define KPLIB_ACTION_PRIORITY_FOBBOXMOVE            (-832)
+#define KPLIB_ACTION_PRIORITY_TELEPORT              (-833)
 
 //#define KPLIB_ACTION_PRIORITY                       ()
 //#define KPLIB_ACTION_PRIORITY                       ()
