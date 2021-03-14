2f1d3227965b1c61095ccdfeadcbf2ff04891cbd
Michael W. Powell <mwpowelllde@gmail.com>
2021-03-14 18:15:51 -0400

    Minor correction.

--- Missionframework/modules/0450_logisticsMgr/ui/KPLIB_logisticsMgr.hpp
+++ Missionframework/modules/0450_logisticsMgr/ui/KPLIB_logisticsMgr.hpp
@@ -4,7 +4,7 @@
     File: KPLIB_logisticsMgr.hpp
     Author: Michael W. Powell [22nd MEU SOC]
     Created: 2021-02-26 18:57:14
-    Last Update: 202021-03-14 18:05:41
+    Last Update: 2021-03-14 18:05:41
     License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
 
     Description:
