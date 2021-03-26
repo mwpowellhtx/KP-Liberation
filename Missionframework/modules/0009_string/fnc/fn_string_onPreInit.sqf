c9139e393d9d7742bca814b72a2e2db45b568bc3
Michael W. Powell <mwpowelllde@gmail.com>
2021-03-26 12:27:59 -0400

    Added STRING module.

---
+++ Missionframework/modules/0009_string/fnc/fn_string_onPreInit.sqf
@@ -0,0 +1,24 @@
+/*
+    KPLIB_fnc_string_onPreInit
+
+    File: fn_string_onPreInit.sqf
+    Author: Michael W. Powell [22nd MEU SOC]
+    Created: 2021-03-26 07:50:51
+    Last Update: 2021-03-26 07:50:54
+    License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
+    Public: Yes
+
+    Description:
+
+    Parameter(s):
+
+    Returns:
+
+    Reference:
+*/
+
+["Initializing...", "PRE] [STRING", true] call KPLIB_fnc_common_log;
+
+["Initialized", "PRE] [STRING", true] call KPLIB_fnc_common_log;
+
+true;
---
+++ Missionframework/modules/0009_string/functions.hpp
@@ -0,0 +1,29 @@
+/*
+    KP LIBERATION MODULE FUNCTIONS
+
+    File: functions.hpp
+    Author: Michael W. Powell [22nd MEU SOC]
+    Created: 2021-03-26 07:48:05
+    Last Update: 2021-03-26 07:48:07
+    License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
+
+    Description:
+        Defines for all functions, which are brought by this module.
+*/
+
+class string {
+    file = "modules\0009_string\fnc";
+
+    // Module initialization event handler
+    class string_onPreInit {
+        preInit = 1;
+    };
+
+    // Module initialization event handler
+    class string_onPostInit {
+        postInit = 1;
+    };
+
+    // Askes whether one string STARTSWITH a substring
+    class string_startsWith {};
+};
