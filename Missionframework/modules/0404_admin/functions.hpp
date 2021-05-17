ce407014f3a198470731bc564647993bcd788c1f
Michael W. Powell <mwpowelllde@gmail.com>
2021-05-17 19:01:54 -0400

    Renumbered module.

---
+++ Missionframework/modules/0404_admin/functions.hpp
@@ -0,0 +1,49 @@
+/*
+    File: functions.hpp
+    Author: KP Liberation Dev Team - https://github.com/KillahPotatoes
+            Michael W. Powell [22nd MEU SOC]
+    Created: 2018-08-02
+    Last Update: 2021-05-17 13:23:09
+    License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
+
+    Description:
+        Defines for all functions, which are brought by this module.
+ */
+
+class adm {
+    file = "modules\0404_admin\fnc";
+
+    // Module pre initialization
+    class admin_preInit {
+        preInit = 1;
+    };
+
+    // Module post initialization
+    class admin_postInit {
+        postInit = 1;
+    };
+
+    // Sets up the player actions for the module
+    class admin_setupPlayerActions {};
+
+    // Deletes exported data in the players profile
+    class admin_deleteExport {};
+
+    // Exports current save data to the players profile
+    class admin_exportSave {};
+
+    // Imports save data from the players profile
+    class admin_importSave {};
+
+    // Applies provided save data for import to the server profile
+    class admin_importSaveServer {};
+
+    // Opens the admin menu dialog
+    class admin_openDialog {};
+
+    // Export current campaign to admins profile and requests server to wipe the saved campaign
+    class admin_wipe {};
+
+    // An admin shorthand while vetting, debugging, etc
+    class admin_onLiberateSectors {};
+};
---
+++ Missionframework/modules/0404_admin/ui/defines.hpp
@@ -0,0 +1,17 @@
+/*
+    KP Liberation admin dialog definitions
+
+    File: defines.hpp
+    Author: Michael W. Powell [22nd MEU SOC]
+    Created: 2021-02-06 10:44:45
+    Last Update: 2021-02-06 10:44:47
+    License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
+
+    Description:
+        Module dialog and control identifier definitions.
+*/
+
+#define KPLIB_IDD_ADMIN                     768020
+
+#define KPLIB_IDC_ADMIN_CTRL_IMPORTBUTTON   768021
+#define KPLIB_IDC_ADMIN_CTRL_DELETEBUTTON   768022
