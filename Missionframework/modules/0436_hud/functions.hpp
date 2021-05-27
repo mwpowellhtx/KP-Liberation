d5db2928c8736589e0ebb705d7b80e084a8bcf15
Michael W. Powell <mwpowelllde@gmail.com>
2021-05-27 15:39:44 -0400

    Updated the HUD functions. Pared back the core HUD functions. Removed unnecessary classes and functions.

--- Missionframework/modules/0436_hud/functions.hpp
+++ Missionframework/modules/0436_hud/functions.hpp
@@ -2,7 +2,7 @@
     File: functions.hpp
     Author: Michael W. Powell [22nd MEU SOC]
     Created: 2021-04-03 00:31:59
-    Last Update: 2021-05-26 22:27:39
+    Last Update: 2021-05-27 14:27:05
     License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
     Public: No
 
@@ -22,211 +22,37 @@
 // TODO: TBD: i.e. present one forward list view, and a backing shadow list view, to give it that stood out effect
 // TODO: TBD: or perhaps we can add 'shadow' to the text font? TBD...
 
-// class hud {
-//     file = "modules\0436_hud\fnc";
+class hud {
+    file = "modules\0436_hud\fnc";
 
-//     // Module initialization phase event handler
-//     class hud_onPreInit {
-//         preInit = 1;
-//     };
+    // Prepares the module preset variables
+    class hud_presets {};
 
-//     // Module initialization phase event handler
-//     class hud_onPostInit {
-//         postInit = 1;
-//     };
+    // Prepares the module CBA settings
+    class hud_settings {};
 
-//     // Gets whether the caller should debug
-//     class hud_debug {};
+    // Module initialization phase event handler
+    class hud_onPreInit {
+        preInit = 1;
+    };
 
-//     // Prepares the module settings
-//     class hud_settings {};
+    // Module initialization phase event handler
+    class hud_onPostInit {
+        postInit = 1;
+    };
 
-//     // Setup player actions
-//     class hud_setupPlayerActions {};
+    // Returns the HUD report object
+    class hud_getReport {};
 
-//     // Checks the PLAYER HUD STATUS; prefers DISPATCH, but may specify either
-//     class hud_checkPlayerStatus {};
+    // Handles the internal HUD REPORT scaffold during regular WAE periods
+    class hud_onReport {};
 
-//     // Conditionally sets a HUD mask the PLAYER HUD STATUS; prefers DISPATCH, but may specify either
-//     class hud_setPlayerStatus {};
+    // Returns whether the REPORT is ALIGNED with an EXPECTED UUID
+    class hud_aligned {};
 
-//     // Conditionally unsets a HUD mask the PLAYER HUD STATUS; prefers DISPATCH, but may specify either
-//     class hud_unsetPlayerStatus {};
-
-//     // Returns whether the elements are in range of each other; favors FOB elements by default
-//     class hud_inRange {};
-
-//     // Returns whether the SECTOR elements are in range of each other; designed specifically for SECTOR concerns
-//     class hud_sectorInRange {};
-
-//     // Refreshes the specified PLAYER TIMER; prefers DISPATCH, but may specify either
-//     class hud_onRefreshPlayerTimer {};
-
-//     // Returns whether the PLAYER TIMER has elapsed; prefers DISPATCH, but may specify either
-//     class hud_hasPlayerTimerElapsed {};
-
-//     // HUD overlay 'onLoad' event handler
-//     class hud_onLoad {};
-
-//     // FOB CT_LISTNBOX 'onLoad' event handler
-//     class hud_lnbFob_onLoad {};
-
-//     // FOB CT_LISTNBOX 'onRefresh' event handler
-//     class hud_lnbFob_onRefresh {};
-
-//     // Responds to CBA waited callback
-//     class hud_onShow {};
-
-//     // Responds to FOB show CBA waited callbacks
-//     class hud_onShowFob {};
-
-//     // Responds to FOB show CBA waited callbacks
-//     class hud_onShowSector {};
-
-//     // Reconciles OVERLAY HASHMAP with an incoming DISPATCH REPORT
-//     class hud_onReconcileOverlayMap {};
-
-//     // Gets the OVERLAY VALUES corresponding to the known HUDDISPATCHSM view data keys
-//     class hud_getFobViewData {};
-
-//     // SECTOR HUD overlay 'onLoad' event handler
-//     class hudSector_onLoad {};
-
-//     // Gets the VIEW DATA for the caller, very generic, and focused for SECTOR REPORT bits
-//     class hudSector_getViewData {};
-
-//     //
-//     class hudSector_ctrlsGrpSector_lblProgressBar_onLoad {};
-
-//     //
-//     class hudSector_ctrlsGrpSector_lblProgressBar_onRefresh {};
-
-//     //
-//     class hudSector_ctrlsGrpSector_lblSectorText_onLoad {};
-
-//     //
-//     class hudSector_ctrlsGrpSector_lblSectorText_onRefresh {};
-
-//     //
-//     class hudSector_ctrlsGrpSector_lblTimer_onLoad {};
-
-//     //
-//     class hudSector_ctrlsGrpSector_lblTimer_onRefresh {};
-
-//     // SECTOR HUD progressSetPosition API, similar to the primitive
-//     class hudSector_ctrlsGrpSector_progressSetPosition {};
-// };
-
-// // TODO: TBD: walking away from server side SM...
-// class hudSM {
-//     file = "modules\0436_hud\statemachine";
-
-//     // Module initialization phase event handler
-//     class hudSM_onPreInit {
-//         preInit = 1;
-//     };
-
-//     // Module initialization phase event handler
-//     class hudSM_onPostInit {
-//         postInit = 1;
-//     };
-
-//     // Returns whether the caller should perform debugging
-//     class hudSM_debug {};
-
-//     // Initializes the module settings
-//     class hudSM_settings {};
-
-//     // Creates the CBA HUD DISPATCH state machine and initializes
-//     class hudSM_createSM {};
-
-//     // Event handler responding to stacked 'PlayerConnected' events
-//     class hudSM_onPlayerConnected {};
-
-//     // Event handler responding to stacked 'PlayerDisconnected' events
-//     class hudSM_onPlayerDisconnected {};
-
-//     // Returns the next iteration of the HUD DISPATCH state machine list
-//     class hudSM_onGetList {};
-
-//     // Performs a no-op for use when tracing and troubleshooting the HUD DISPATCH statemachine
-//     class hudSM_onNoOp {};
-
-//     //
-//     class hudSM_onStandbyEntered {};
-
-//     // STANDBY 'onState' event handler, briefs PLAYER with next REPORT and STATUS
-//     class hudSM_onStandby {};
-
-//     // Compiles the next STATUS REPORT 'onTransition' between STANDBY and DISPATCH
-//     class hudSM_onCompileReport {};
-
-//     // Creates a REPORT CONTEXT for use throughout the report callbacks
-//     class hudSM_createReportContext {};
-
-//     // Initializes a REPORT CONTEXT for FOB usage
-//     class hudSM_onCreateReportContext_initFob {};
-
-//     // Initializes a REPORT CONTEXT for SECTOR usage
-//     class hudSM_onCreateReportContext_initSector {};
-
-//     // Dispatches the STANDBY REPORT to the target PLAYER when there is something different to report
-//     class hudSM_onDispatchEntered {};
-
-//     // Returns the ASSETS in RANGE of the FOBs that SHOULD be COUNTED
-//     class hudSM_getFobAssets {};
-
-//     // Returns UNITS within RANGE of a POSITION; default scenario is in range of FOB
-//     class hudSM_getUnits {};
-
-//     //
-//     class hudSM_getFobUnits {};
-
-//     // Returns UNITS within RANGE of POSITION aligned to a given SIDE
-//     class hudSM_getSectorUnits {};
-
-//     // Renders the SCALAR in a flexible manner
-//     class hudSM_renderScalar {};
-
-//     // Scans [CONDITION, COLOR] tuple array for the first CONDITION and returns its COLOR, default is BLACK
-//     class hudSM_getThresholdColor {};
-
-//     // Compiles a FOB REPORT targeting ASSET counts
-//     class hudSM_onReportFob_assets {};
-
-//     // Compiles a FOB REPORT targeting enemy AWARENESS and STRENGTH ratings, includes rendered PERCENTAGE and COLOR
-//     class hudSM_onReportFob_enemy {};
-
-//     // Compiles a FOB REPORT targeting CIVILIAN REPUTATION, includes COLOR
-//     class hudSM_onReportFob_civRep {};
-
-//     // Compiles a FOB REPORT targeting INTEL, includes COLOR
-//     class hudSM_onReportFob_intel {};
-
-//     // Compiles a FOB REPORT targeting MARKER TEXT
-//     class hudSM_onReportFob_markerText {};
-
-//     // Compiles a FOB REPORT targeting RESOURCE counts
-//     class hudSM_onReportFob_resources {};
-
-//     // Compiles a FOB REPORT targeting non player UNIT count
-//     class hudSM_onReportFob_units {};
-
-//     //
-//     class hudSM_onReportSector_colors {};
-
-//     // Compiles a SECTOR REPORT targeting GRIDREF
-//     class hudSM_onReportSector_gridref {};
-
-//     // Compiles a SECTOR REPORT targeting MARKER TEXT
-//     class hudSM_onReportSector_markerText {};
-
-//     // Compiles a SECTOR REPORT tarting PROGRESS BAR POSITION
-//     class hudSM_onReportSector_progressBar {};
-
-//     //
-//     class hudSM_onReportSector_timer {};
-// };
+    // Subscribes to a HUD REPORT and hands off a WUAE+WAE event baton
+    class hud_subscribe {};
+};
 
 // TODO: TBD: migrating toward client side WAE style event loops...
 class hudFob {
@@ -247,9 +73,6 @@ class hudFob {
     class hudFob_onPostInit {
         postInit = 1;
     };
-
-    // Returns the next FOB HUD report object
-    class hudFob_getReport {};
 
     // Fleshes out the next REPORT and directs HUD resource attention
     class hudFob_onReport {};
