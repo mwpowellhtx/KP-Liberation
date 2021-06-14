98ed4093961ba61c5303efa165f86aa27f233c9f
Michael W. Powell <mwpowelllde@gmail.com>
2021-06-14 16:58:54 -0400

    Revised sectors state machine.

--- Missionframework/modules/0412_sectors/statemachine/script_component.hpp
+++ Missionframework/modules/0412_sectors/statemachine/script_component.hpp
@@ -2,7 +2,7 @@
     File: script_component.hpp
     Author: Michael W. Powell [22nd MEU SOC]
     Created: 2021-04-03 15:43:27
-    Last Update: 2021-05-17 14:52:38
+    Last Update: 2021-06-14 16:56:23
     License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
     Public: No
 
@@ -11,18 +11,18 @@
  */
 
 #define MODULE sectors
-#define MODULESM sectorsSM
+#define MODULESM sectorSM
 
 #define Q(x) #x
 
-#define MPRESET(x) KPLIB_preset_##MODULE##x
-#define QMPRESET(x) Q(MPRESET(x))
+#define MPRESET(var) KPLIB_preset_##MODULE##var
+#define QMPRESET(var) Q(MPRESET(var))
 
-#define MPARAM(x) KPLIB_param_##MODULE##x
-#define QMPARAM(x) Q(MPARAM(x))
+#define MPARAM(var) KPLIB_param_##MODULE##var
+#define QMPARAM(var) Q(MPARAM(var))
 
-#define MSTATUS(x) KPLIB_##MODULE##_status##x
-#define QMSTATUS(x) Q(MSTATUS(x))
+#define MSTATUS(var) KPLIB_##MODULE##_status##var
+#define QMSTATUS(var) Q(MSTATUS(var))
 
 #define MVAR(var) KPLIB_##MODULE##var
 #define QMVAR(var) Q(MVAR(var))
